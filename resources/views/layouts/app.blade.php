b8f1164bc7e56ada16919a661f93e5cde1006830
Arjeta Avllaj <arjeta.avllaj14@gmail.com>
2018-04-04 12:38:54 +0200

    Users management functionallity (part 2)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css" rel="stylesheet">
 </head>
 <body>
--- resources/views/manage/users/index.blade.php
+++ resources/views/manage/users/index.blade.php
@@ -7,36 +7,36 @@
                 <h1 class="title">Manage Users</h1>
             </div>
             <div class="column">
-                <a href="{{route('users.create')}}" class="button is-success"><i class="fas fa-user-plus"></i>Add new user</a>
+                <a href="{{route('users.create')}}" class="button is-success"><i class="fas fa-user-plus"></i>Add new
+                    user</a>
             </div>
         </div>
         <hr>
-        <div class="card">
-            <div class="card-content">
-        <table class="table">
-            <thead>
-            <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Email ddress</th>
-                <th>Date created</th>
-                <th>Actions</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($users as $user )
-            <tr>
-                <th>{{$user->id}}</th>
-                <td>{{$user->name}}</td>
-                <td>{{$user->email}}</td>
-                <td>{{$user->created_at}}</td>
-                <td><a class="button is-outlined" href="{{route('users.edit', $user->id)}}">Edit</a></td>
-            @endforeach
-            </tbody>
-        </table>
-    </div>
-    </div>
-    </div>
 
+        <div class="card-content">
+            <table class="table is-striped is-narrow is-fullwidth">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Username</th>
+                    <th>Email ddress</th>
+                    <th>Date created</th>
+                    <th>Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($users as $user )
+                    <tr>
+                        <th>{{$user->id}}</th>
+                        <td>{{$user->name}}</td>
+                        <td>{{$user->email}}</td>
+                        <td>{{$user->created_at}}</td>
+                        <td><a class="button is-outlined" href="{{route('users.edit', $user->id)}}">Edit</a></td>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+{{$users->links()}}
 
 @endsection
